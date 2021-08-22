515a92130837bd73832eb8e5092a2497a822fb49
Adam Watkins <adam@stupidpupil.co.uk>
2021-08-22 13:10:19 +0100

    Change license link

--- R/write_osrm_matrix.R
+++ R/write_osrm_matrix.R
@@ -9,7 +9,7 @@ write_osrm_matrix <- function(){
     time_ref_type = "arrive_by",
     time_ref = lubridate::now(tz="Europe/London") %>% update(hours=9, minutes=0, seconds=0) %>% lubridate::format_ISO8601(usetz = TRUE),
     license = "ODbL-1.0",
-    license_ref = "https://github.com/stupidpupil/wales_ish_osrm_runner/blob/main/README.md#licence-of-outputs"
+    license_ref = "https://github.com/stupidpupil/wales_ish_osrm_runner/tree/matrix-releases#licence"
   ))
 
 }
